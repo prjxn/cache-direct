3cbb65c7520b24efecb1dd85e921b6af21dd7a0d
Arjun P <prjxn0@gmail.com>
2025-11-22 12:48:12 +0530

    Update cache_mem.v

--- rtl/cache_mem.v
+++ rtl/cache_mem.v
@@ -7,14 +7,17 @@ output reg [7:0] r_data;
 reg [7:0] mem [0:7];
 integer i;
 
-always@(negedge clk) if (wr && ~rst) mem[addr] <= w_data ;
+always@(negedge clk) begin
+if(rst) for (i=0;i<8;i=i+1) mem[i] <= 8'b0;
+else if (wr) mem[addr] <= w_data;
+end
 
 always@(posedge clk or posedge rst) begin 
 if(rst)	begin
-	for (i=0;i<8;i=i+1) mem[i] <= 8'b0;
 	r_data <= 8'b0;
 end
 else begin
 	if (rd) r_data <= mem[addr];
 end end
 endmodule
+
